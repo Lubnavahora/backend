7245c367e49566837cf5503a84a32905590dcd2d
Lubnavahora <n11660503@qut.edu.au>
2025-04-02 14:07:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/api/enrollments', enrollmentRoutes);
 
 // Start server only if this file is run directly
 if (require.main === module) {
-    const PORT = process.env.PORT || 5002;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 }
 
